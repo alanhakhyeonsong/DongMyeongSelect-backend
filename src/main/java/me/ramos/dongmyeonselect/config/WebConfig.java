fdfe0e792877de91dc351eabfbaacecb17ae09a2
alanhakhyeonsong <songs4805@naver.com>
2022-02-28 16:17:27 +0900

    CORS issue 해결

--- src/main/java/me/ramos/dongmyeonselect/config/WebConfig.java
+++ src/main/java/me/ramos/dongmyeonselect/config/WebConfig.java
@@ -8,8 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("/*")
+        registry.addMapping("/**")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST");
     }
 }
